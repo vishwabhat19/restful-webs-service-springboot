13f5d7a2cfe673b1d1ab43bab1b53395a887a101
Vishwa <VB086617>
2022-03-26 17:16:45 +0530

    Adding Generalised Exception Handlin g

--- src/main/java/com/rest/webservice/restfulwebservice/exceptions/UserNotFoundException.java
+++ src/main/java/com/rest/webservice/restfulwebservice/exceptions/UserNotFoundException.java
@@ -1,5 +1,9 @@
 package com.rest.webservice.restfulwebservice.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException{
 
     public UserNotFoundException(String message) {
