52131325c9bf3172167eade45506304bef542c25
Vishwa <VB086617>
2022-03-27 22:15:38 +0530

    Adding XML output depedency by jackson-dataformat-xml(content-negotiation)

--- src/main/java/com/rest/webservice/restfulwebservice/controller/RestfulWebServiceController.java
+++ src/main/java/com/rest/webservice/restfulwebservice/controller/RestfulWebServiceController.java
@@ -1,8 +1,10 @@
 package com.rest.webservice.restfulwebservice.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.net.URI;
 import java.util.List;
-import java.util.Locale;
 
 import javax.validation.Valid;
 
@@ -21,10 +23,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 public class RestfulWebServiceController {
